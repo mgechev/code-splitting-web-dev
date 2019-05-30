80ed8c4f26c15d0c3ee1e241096a5b96a964e7f0
mgechev <mgechev@gmail.com>
2019-05-30 15:50:41 +0200

    Move home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
-import { HomeComponent } from '../home/home.component';
+import { HomeComponent } from './home/home.component';
 import { NyanComponent } from './nyan/nyan.component';
 
 @NgModule({
